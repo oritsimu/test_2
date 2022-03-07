0afc9cac67feb8433babf0ddd8278df05b8df4ca
oritsimu <76267927+oritsimu@users.noreply.github.com>
2022-03-07 15:10:26 +0300

    Update Helpers.py

--- model/Helpers.py
+++ model/Helpers.py
@@ -1,47 +1,66 @@
 
 class Helpers:
+    
+    @staticmethod
+    def clean(keyword):
+        
+        restricted_characters = ['-', ',', '\'', ')', '(', '[', ']', '{', '}', '.', '*', '?', '_', '@', '!', '$', '&', '#', '~', '%']
+        
+        clean_keyword = ""
+        for char in keyword:
+            if char not in restricted_characters:
+                clean_keyword += char
 
+        white_space_counter = 0
+
+        for char in clean_keyword:
+            if char == ' ':
+                white_space_counter += 1
+            else:
+                break
+
+        clean_keyword = clean_keyword[white_space_counter:]
+
+        white_space_counter = 0
+
+        for i in range(len(clean_keyword) - 1, 0, -1):
+            if clean_keyword[i] == ' ':
+                white_space_counter += 1
+            else:
+                break
+
+        if white_space_counter != 0:
+            clean_keyword = clean_keyword[:-white_space_counter]
+
+        return clean_keyword
+
+        
     @staticmethod
     def removeRestrictedCharactersAndWhiteSpaces(keywords):
 
         keywords = list(dict.fromkeys(keywords))  # Remove dupes
         keywords = list(filter(None, keywords))  # Remove empty
 
-        restricted_characters = ['-', ',', '\'', ')', '(', '[', ']', '{', '}', '.', '*', '?', '_', '@', '!', '$', '&', '#', '~', '%']
-
         preprocessed_list = []
 
-        for keyword in keywords:
-
-            clean_keyword = ""
-            for char in keyword:
-                if char not in restricted_characters:
-                    clean_keyword += char
-
-            white_space_counter = 0
-
-            for char in clean_keyword:
-                if char == ' ':
-                    white_space_counter += 1
-                else:
-                    break
-
-            clean_keyword = clean_keyword[white_space_counter:]
-
-            white_space_counter = 0
-
-            for i in range(len(clean_keyword) - 1, 0, -1):
-                if clean_keyword[i] == ' ':
-                    white_space_counter += 1
-                else:
-                    break
-
-            if white_space_counter != 0:
-                clean_keyword = clean_keyword[:-white_space_counter]
-
-            preprocessed_list.append(clean_keyword)
-
+        for keyword_geo in keywords:
+            
+            if "-" in keyword_geo:
+                keyword = keyword_geo.split("-")[0]
+                geos = keyword_geo.split("-")[1].split(" ")
+                geos = [e for e in geos if e != " " and e != ""]
+            
+                keyword = Helpers.clean(keyword)
+            
+                for geo in geos:
+                    geo = Helpers.clean(geo)
+                    preprocessed_list.append(keyword + "-" + geo)
+            else:
+                keyword = Helpers.clean(keyword_geo)
+                preprocessed_list.append(keyword)
+            
         return preprocessed_list
+            
 
     @staticmethod
     def updateCredentials(refresh_token, client_secret, client_id, developer_token, login_customer_id):
@@ -49,6 +68,7 @@ class Helpers:
         cred_data = f.read()
         f.close()
         cred_data_splitted = cred_data.split("\n")
+
 
         new_txt = ""
 
@@ -69,14 +89,3 @@ class Helpers:
         f = open("auth/credentials.yaml", 'w')
         f.write(new_txt)
         f.close()
-
-
-
-
-
-
-
-
-
-
-#END
